4e7a56a6590215477874e22c7ae0afce7a5e7888
Kevin Hamer <kevin@imarc.com>
2016-04-19 10:25:19 -0400

    renaming stickIndex to make it clearer

--- js/cards.js
+++ js/cards.js
@@ -6,7 +6,7 @@
  */
 
 var cards = function(selector) {
-    var stickIndex = 0;
+    var startSticking = 0;
     var activeOpacity = 0;
     var container = document.querySelector(selector);
     var cards = [].slice.call(container.children);
@@ -19,26 +19,25 @@ var cards = function(selector) {
         }
     };
 
-	var stickCards = function() {
-		for(var i = stickIndex; i < cards.length; i++) {
-			cards[i].style.position = 'fixed';
-			cards[i].style.top = '1px';
-			cards[i].style.opacity = i === stickIndex ? activeOpacity : 0;
-		}
-	};
-
 	var unstickCards = function() {
-		for(var i = 0; i < stickIndex; i++) {
+		for(var i = 0; i < startSticking; i++) {
 			cards[i].style.opacity = 1;
 			cards[i].style.position = 'absolute';
 			cards[i].style.top = (i * 100) + 'vh';
 		}
 	};
 
+	var stickCards = function() {
+		for(var i = startSticking; i < cards.length; i++) {
+			cards[i].style.position = 'fixed';
+			cards[i].style.top = '1px';
+			cards[i].style.opacity = i === startSticking ? activeOpacity : 0;
+		}
+	};
+
     var updateCards = function() {
         var scroll = (window.pageYOffset - container.offsetTop) / window.innerHeight;
-        console.log(scroll);
-        stickIndex = Math.max(0, Math.floor(scroll) + 1);
+        startSticking = Math.max(0, Math.floor(scroll) + 1);
         activeOpacity = scroll % 1;
 
         unstickCards();
@@ -48,7 +47,6 @@ var cards = function(selector) {
     };
 
     initializeZIndex();
-    stickCards(cards);
     container.style.height = (cards.length * 100)  + 'vh';
     window.requestAnimationFrame(updateCards);
 };
